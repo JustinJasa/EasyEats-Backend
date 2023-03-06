8abd884dd878d1750a721bbe0433e3de8b070354
unknown <memogarcia246@gmail.com>
2023-03-06 15:34:56 +0100

    added api functionality for get and post calls regarding recipes

--- controllers/queries.js
+++ controllers/queries.js
@@ -76,13 +76,24 @@ export const getRecipeIngredients = async (recipeId) => {
     return rows
 }
 
-
 // Get steps of a recipe by recipe_id
 export const getRecipeSteps = async (recipeId) => {
     const [rows] = await pool.query(`
     SELECT s.description
     FROM steps s
     WHERE s.recipe_id = ?`, [recipeId])
+
+    return rows
+}
+
+// Get comments of a recipe by recipe_id
+export const getRecipeComments = async (recipeId) => {
+    const [rows] = await pool.query(`
+    SELECT u.username, c.comment, c.rating
+    FROM recipes r
+    INNER JOIN comments c ON r.recipe_id = c.recipe_id
+    INNER JOIN users u ON c.user_id = u.user_id
+    WHERE c.recipe_id = ?`, [recipeId])
 
     return rows
 }
@@ -97,3 +108,42 @@ export const getUserInfo = async (userId) => {
     return row
 }
 
+// Insert basic info of a recipe into recipes table
+export const createRecipe = async (userId, name, description, time_h, time_m, price) => {
+    const [result] = await pool.query(`
+    INSERT INTO recipes (user_id, name, description, time_hours, time_minutes, price_range)
+    VALUES (?, ?, ?, ?, ?, ?)`, [userId, name, description, time_h, time_m, price])
+
+    const id = result.insertId
+    return getRecipeInfo(id)
+}
+
+// Insert categories of a recipe into recipe_categories table
+export const createRecipeCategories = async (recipeId, categoryId) => {
+    const [result] = await pool.query(`
+    INSERT INTO recipe_categories (recipe_id, category_id)
+    VALUES (?, ?)`, [recipeId, categoryId])
+
+    const id = result.insertId
+    return getRecipeCategories(recipeId)
+}
+
+// Insert ingredients of a recipe into ingredients table
+export const createRecipeIngredients = async (recipeId, description) => {
+    const [result] = await pool.query(`
+    INSERT INTO ingredients (recipe_id, description)
+    VALUES (?, ?)`, [recipeId, description])
+
+    const id = result.insertId
+    return getRecipeIngredients(id)
+}
+
+// Insert steps of a recipe into steps table
+export const createRecipeSteps = async (recipeId, description) => {
+    const [result] = await pool.query(`
+    INSERT INTO steps (recipe_id, description)
+    VALUES (?, ?)`, [recipeId, description])
+
+    const id = result.insertId
+    return getRecipeSteps(id)
+}
