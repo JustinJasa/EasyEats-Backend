f41ee4c93f96d12765df82217f79df722447fcf6
unknown <memogarcia246@gmail.com>
2023-03-09 22:26:41 +0100

    added rest of the queries

--- controllers/queries.js
+++ controllers/queries.js
@@ -12,7 +12,7 @@ export const getAllCategories = async () => {
 // Get basic info (recipeName, user) of ALL recipes
 export const getAllRecipes = async () => {
     const [rows] = await pool.query(`
-    SELECT r.name, u.username
+    SELECT r.recipe_id, r.name, u.username
     FROM users u
     INNER JOIN recipes r ON u.user_id = r.user_id`)
     
@@ -22,7 +22,7 @@ export const getAllRecipes = async () => {
 // Get basic info (recipeName, user) of recipes by categoryName
 export const getRecipesByCategoryName = async (categoryName) => {
     const [rows] = await pool.query(`
-    SELECT r.name, u.username
+    SELECT r.recipe_id, r.name, u.username
     FROM users u
     INNER JOIN recipes r ON u.user_id = r.user_id
     INNER JOIN recipe_categories rc ON r.recipe_id = rc.recipe_id
@@ -35,7 +35,7 @@ export const getRecipesByCategoryName = async (categoryName) => {
 // Get basic info (recipeName, user) of recipes by recipeName
 export const getRecipesByRecipeName = async (recipeName) => {
     const [rows] = await pool.query(`
-    SELECT r.name, u.username
+    SELECT r.recipe_id, r.name, u.username
     FROM users u
     INNER JOIN recipes r ON u.user_id = r.user_id
     WHERE r.name LIKE '%?%'`, [recipeName])
@@ -89,13 +89,21 @@ export const getRecipeSteps = async (recipeId) => {
 // Get comments of a recipe by recipe_id
 export const getRecipeComments = async (recipeId) => {
     const [rows] = await pool.query(`
-    SELECT u.username, c.comment, c.rating
+    SELECT c.comment_id, u.username, c.comment, c.rating
     FROM recipes r
     INNER JOIN comments c ON r.recipe_id = c.recipe_id
     INNER JOIN users u ON c.user_id = u.user_id
     WHERE c.recipe_id = ?`, [recipeId])
 
     return rows
+}
+
+// Get a single comment by comment_id
+export const getComment = async (commentId) => {
+    const [result] = await pool.query(`
+    SELECT *
+    FROM comments c
+    WHERE c.comment_id = ?`, [commentId])
 }
 
 // Get user information by user_id
@@ -148,7 +156,27 @@ export const createRecipeSteps = async (recipeId, description) => {
     return getRecipeSteps(id)
 }
 
-// Insert steps of a recipe into steps table
+// Insert a new comment into comments table
+export const createComment = async (userId, recipeId, comment, rating) => {
+    const [result] = await pool.query(`
+    INSERT INTO comments (user_id, recipe_id, comment, rating)
+    VALUES (?, ?, ?, ?)`, [userId, recipeId, comment, rating])
+
+    const id = result.insertId
+    return getComment(id)
+}
+
+// Insert a new user into users table
+export const createUser = async (username, email, password) => {
+    const [result] = await pool.query(`
+    INSERT INTO users (username, email, password)
+    VALUES (?, ?, ?)`, [username, email, password])
+
+    const id = result.insertId
+    return getUserInfo(id)
+}
+
+// Update basic info of a recipe in recipes table
 export const updateRecipeInfo = async (recipeId, name, description, time_h, time_m, price) => {
     const [result] = await pool.query(`
     UPDATE recipes
@@ -163,6 +191,75 @@ export const updateRecipeInfo = async (recipeId, name, description, time_h, time
     return getRecipeInfo(recipeId)
 }
 
+// Update comment information in comments table
+export const updateComment = async (commentId, comment, rating) => {
+    const [result] = await pool.query(`
+    UPDATE comments
+    SET
+        comment = ?,
+        rating = ?
+    WHERE comment_id = ?`, [comment, rating, commentId])
+
+    return getComment(commentId)
+}
+
+// Update user information in users table
+export const updateUser = async (userId, username, password) => {
+    const [result] = await pool.query(`
+    UPDATE users
+    SET
+        username = ?,
+        password = ?
+    WHERE user_id = ?`, [username, password, userId])
+
+    return getUserInfo(userId)
+}
+
+// Delete a recipe from recipes table. Also deletes its associated comments in comments table
+export const deleteRecipe = async (recipeId) => {
+    // First delete comments from comments table
+    const [resultDelComments] = await pool.query(`
+    DELETE FROM comments
+    WHERE recipe_id = ?`, [recipeId])
+
+    // Then delete recipe from recipes table
+    const [resultDelRecipe] = await pool.query(`
+    DELETE FROM recipes
+    WHERE recipe_id = ?`, [recipeId])
+}
+
+// Delete comment from comments table
+export const deleteComment = async (commentId) => {
+    const [result] = await pool.query(`
+    DELETE FROM comments
+    WHERE comment_id = ?`, [commentId])
+}
+
+// Delete a user from users table. Also deletes user's associated recipes and comments
+export const deleteUser = async (userId) => {
+    // First delete user's comments
+    const [resultDelComments] = await pool.query(`
+    DELETE FROM comments
+    WHERE user_id = ?`, [userId])
+
+    // Get a hold of the recipes the user has
+    const [userRecipes] = await pool.query(`
+    SELECT r.recipe_id
+    FROM recipes r
+    WHERE r.user_id = ?`, [userId])
+
+    // Iterate over user's recipes and delete them
+    for(let i = 0; i< userRecipes.length; i++) {
+        const recipeId = userRecipes[i]
+        deleteRecipe(recipeId)
+    }
+
+    // Delete user
+    const [resultDelUser] = await pool.query(`
+    DELETE FROM users
+    WHERE user_id = ?`, [userId])
+}
+
 // Delete all categories of a recipe by recipeId
 export const deleteRecipeCategories = async (recipeId) => {
     const [result] = await pool.query(`
@@ -170,12 +267,14 @@ export const deleteRecipeCategories = async (recipeId) => {
     WHERE recipe_id = ?`, [recipeId])
 }
 
+// Delete all ingredients of a recipe by recipeId
 export const deleteRecipeIngredients = async (recipeId) => {
     const [result] = await pool.query(`
     DELETE FROM ingredients
     WHERE recipe_id = ?`, [recipeId])
 }
 
+// Delete all steps of a recipe by recipeId
 export const deleteRecipeSteps = async (recipeId) => {
     const [result] = await pool.query(`
     DELETE FROM steps
