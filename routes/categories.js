39f6785db30df440ee6a4cc9676a205fe6444e9f
unknown <memogarcia246@gmail.com>
2023-03-12 16:23:00 +0100

    remove unused code

--- routes/categories.js
+++ routes/categories.js
@@ -9,19 +9,4 @@ routerCategories.route("/").get(async (req, res) => {
     res.send(queryResult)
 });
 
-// get a particular category
-routerCategories.route("/:id").get((req,res) => {
-    let categoryId = req.params.id
-    res.send(`getting category information for ${categoryId}`)
-})
-
-// create a new category and add to the categories
-routerCategories.route("/").post((req,res) => {
-    res.send("created a new category")
-    console.log(req.body)
-    res.json({
-        status: 'success'
-    })
-})
-
 export default routerCategories
