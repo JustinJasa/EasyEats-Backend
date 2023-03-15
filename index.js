b96093d573bd5229224851769611858c727f3292
Justin Jasa <jasa@Justins-MacBook-Pro-2.local>
2023-03-15 18:08:44 +0100

    adding middleware to check jwt tokens

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import routerRecipes from './routes/recipes.js'
 import routerUsers from './routes/users.js'
 import routerAuth from './routes/auth.js'
 import cors from 'cors'
+import checkJwt from './middleware/tokenAuth.js'
 
 const PORT = process.env.PORT || 8000
 
@@ -12,9 +13,9 @@ app.use(cors())
 
 app.use(express.json())
 
-app.use('/categories', routerCategories)
-app.use('/recipes', routerRecipes)
-app.use('/users', routerUsers)
+app.use('/categories', checkJwt, routerCategories)
+app.use('/recipes',  checkJwt, routerRecipes)
+app.use('/users',  checkJwt, routerUsers,)
 app.use('/auth', routerAuth)
 
 
