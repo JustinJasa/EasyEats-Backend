4bc46ae6e1af56ec2d6a303f1509d8e521544534
unknown <memogarcia246@gmail.com>
2023-03-26 16:13:38 +0200

    update db creation code and initial data

--- database.sql
+++ database.sql
@@ -3,8 +3,6 @@ CREATE TABLE users(
     username VARCHAR(255),
     email VARCHAR(255),
     password VARCHAR(255),
-    picture_name VARCHAR(255),
-    picture_path VARCHAR(255)
 );
 
 CREATE TABLE recipes(
@@ -15,6 +13,7 @@ CREATE TABLE recipes(
     time_hours INT,
     time_minutes INT,
     price_range VARCHAR(3), -- Can be $, $$, or $$$
+    date DATE,
 
     FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE ON UPDATE NO ACTION
 );
@@ -63,7 +62,6 @@ CREATE TABLE comments(
     user_id INT,
     recipe_id INT,
     comment VARCHAR(1024),
-    rating INT, -- between 1-5
 
     FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE ON UPDATE NO ACTION,
     FOREIGN KEY (recipe_id) REFERENCES recipes (recipe_id) ON DELETE CASCADE ON UPDATE NO ACTION
@@ -74,10 +72,10 @@ CREATE TABLE comments(
 -- //////////////
 INSERT INTO users (username, email, password)
 VALUES
-    ('Michael Scott', 'michael@gmail.com', 'michael123'),
-    ('John Smith', 'john@gmail.com', 'john456'),
+    ('Walter Hartwell White', 'mrwhite@student.ju.se', 'theOneWhoKnocks'),
+    ('Saul Goodman', 'saul@student.ju.se', 'hooverMaxExtractPressureProModel60'),
     ('Jesse Pinkman', 'jesse@gmail.com', 'yoyoyo1483369'),
-    ('Lucy Williams', 'lucy@gmail.com', 'lucy789');
+    ('Chuck McGill', 'chicanery@gmail.com', 'iAmNotCrazy');
 
 INSERT INTO categories (name)
 VALUES
@@ -87,73 +85,101 @@ VALUES
     ('Healthy'),
     ('Easy'),
     ('Advanced'),
-    ('Vegetarian');
+    ('Vegetarian'),
+    ('Noodles'),
+    ('Pasta'),
+    ('Italian'),
+    ('Cheap'),
+    ('Chicken'),
+    ('Beef'),
+    ('Pork'),
+    ('Vegan'),
+    ('Quick'),
+    ('Drinks'),
+    ('Protein'),
+    ('Dairy'),
+    ('Dessert'),
+    ('Fruit'),
+    ('Seafood'),
+    ('Cheese'),
+    ('Egg'),
+    ('Alcohol'),
+    ('Lunch'),
+    ('Breakfast');
 
-INSERT INTO recipes (user_id, name, description, time_hours, time_minutes, price_range)
+INSERT INTO recipes (user_id, name, description, time_hours, time_minutes, price_range, date)
 VALUES
-    ((SELECT user_id FROM users WHERE user_id = 1), 'Bacon and eggs', 'Tasty dish that requires few ingredients and is not time consuming', 0, 15, '$'),
-    ((SELECT user_id FROM users WHERE user_id = 1), 'Quesadillas', 'Basic dish from Mexico. Simple, tasty, and quick.', 0, 15, '$'),
-    ((SELECT user_id FROM users WHERE user_id = 2), 'Spaghetti bolognese', 'Delicious food that anyone can make. The taste is very good and it can last for multiple servings', 0, 30, '$$');
+    ((SELECT user_id FROM users WHERE user_id = 1), 'Pasta Carbonara', 'A delicious and authenti Italian pasta 🍝', 0, 45, '$'),
+    ((SELECT user_id FROM users WHERE user_id = 2), 'French Toast', 'Make this and good things will happen 😉💦', 0, 20, '$'),
+    ((SELECT user_id FROM users WHERE user_id = 3), 'Halal Style Chicken and Rice', 'Comfort food thats healthy and packed with protein', 1, 5, '$$');
 
 INSERT INTO ingredients (recipe_id, description)
 VALUES
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), '2 large eggs'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), '3 strips of bacon'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Oil'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Bacon or Guancale'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Pasta of any kind (preferably spaghetti)'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Pecorino or parmesan cheese'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), '3 eggs (per person)'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Salt'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Pepper'),
 
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Corn of flour tortillas'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Shredded cheese of your choice'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'White bread'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Bananas'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Blueberries'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Maple Syrup'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Egg'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Cinnamon'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Flour'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Butter'),
 
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), '200 grams of spaghetti'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), '200 grams of ground beef'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), '1/2 onion, diced'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), '1 tomato, diced'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), '1 jar of Classic tomato pasta sauce'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Oil');
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Butter'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Paprika'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Rice'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Salad (optional)'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Mayo'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Chicken thighs'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Cumin'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Turmeric'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Chicken stock'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Salt'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Garlic powder');
 
 INSERT INTO recipe_categories (recipe_id, category_id)
 VALUES
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), (SELECT category_id FROM categories WHERE category_id = 3)),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), (SELECT category_id FROM categories WHERE category_id = 5)),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), (SELECT category_id FROM categories WHERE category_id = 9)),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), (SELECT category_id FROM categories WHERE category_id = 10)),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), (SELECT category_id FROM categories WHERE category_id = 23)),
     ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), (SELECT category_id FROM categories WHERE category_id = 5)),
 
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), (SELECT category_id FROM categories WHERE category_id = 2)),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), (SELECT category_id FROM categories WHERE category_id = 3)),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), (SELECT category_id FROM categories WHERE category_id = 5)),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), (SELECT category_id FROM categories WHERE category_id = 19)),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), (SELECT category_id FROM categories WHERE category_id = 21)),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), (SELECT category_id FROM categories WHERE category_id = 24)),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), (SELECT category_id FROM categories WHERE category_id = 27)),
 
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), (SELECT category_id FROM categories WHERE category_id = 5));
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), (SELECT category_id FROM categories WHERE category_id = 1)),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), (SELECT category_id FROM categories WHERE category_id = 4)),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), (SELECT category_id FROM categories WHERE category_id = 12)),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), (SELECT category_id FROM categories WHERE category_id = 18));
 
 INSERT INTO steps (recipe_id, description)
 VALUES
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Crack eggs into a small bowl'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Whisk eggs for 30 seconds'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Pour a bit of oil onto 2 pans and set them to medium heat'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Once the pan is hot, place the strips of bacon for 2.5 minutes, then turn and wait the same time'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Pour the eggs onto the other pan and let them sit for 20 seconds'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Use a spatula mix the eggs and fold them over'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'To create the sauce, add 3 egg yolks to half a cup of shredded cheese'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Chop the bacon/Guanciale and add it to the pan'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Crisp up the meat leaving the fat in the pan'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Boil hot water with salt, once boiling add the pasta in'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'When the pasta is cooked add the noodles to the pan with the meat. Mix the noodle and meat together'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Add the egg and cheese sauce, along with some pasta water to combine the sauce'),
     ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Once the eggs are softly set, remove them from the heat'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Remove the bacon from the heat, and also remove their excess grease with a napkin'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Place bacon and eggs on a plate and enjoy'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Top with cheese and pepper and enjoy!'),
 
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Heat up a pan to medium heat'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Once pan is hot, place the tortilla and let it sit for 20 seconds'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Flip the tortilla over'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Place shredded cheese on the tortilla and fold it in half'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Let the folded tortilla sit for 30 seconds and then flip it'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Once the cheese is melted, remove from heat and serve'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Coat your bread in an egg and flour mixture and set aside'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Heat up a pan with butter, and add the bread to the pan'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Pan fry until its nice and golden brown'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Add to a plate and add chop bananas and blueberry with a nice serving of maple syrup'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Devour or serving to someone you care for and enjoy!'),
 
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Pour 750 ml of water into a pot and put it to high heat'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Pour a bit of oil onto a pan and set to medium-high heat'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'When pan is hot, place onions and spread them evenly. Mix for 1 minute'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Place ground beef on pan and use a spoon or spatula to separate it into small pieces'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Mix beef and onions for 2 minutes, then place the diced tomato'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Place the spaghetti into the boiling water, stir from time to time for a total of 8-10 minutes'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Once the spaghetti is almost done, pour the tomato sauce into the pan with the beef, onions, and tomatos. Mix for 1 minute'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Drain the water from the spaghetti pot and place the contents of the pan in it. Mix to combine all ingredients'),
-    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Serve');
-
-INSERT INTO comments (user_id, recipe_id, comment, rating)
-VALUES
-    ((SELECT user_id FROM users WHERE user_id = 2), (SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Thank you very much for sharing this recipe! Very tasty!', 5),
-    ((SELECT user_id FROM users WHERE user_id = 3), (SELECT recipe_id FROM recipes WHERE recipe_id = 1), 'Delicious! I will cook this for my friends!', 5),
-    ((SELECT user_id FROM users WHERE user_id = 4), (SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Good taste! :)', 4),
-    ((SELECT user_id FROM users WHERE user_id = 4), (SELECT recipe_id FROM recipes WHERE recipe_id = 2), 'Very nice, delicious and easy to make!', 5);
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Marinate the chicken in mayo and the spice like paprika, cumin, salt pepper and garlic powder'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'For the rice, toast some tumeric in the pan and until fragrant but not burnt (about 30 seconds)'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Stir in the rice and cook add chicken stock. Cook for about 20 minutes'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Pan fry the chicken until cook. You can cut the chicken and toss in the pan to collect the chicken juices'),
+    ((SELECT recipe_id FROM recipes WHERE recipe_id = 3), 'Serve the chicken and rice and enjoy the meal!');
